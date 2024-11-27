94f2e89bf127cec056b574dcdb1251c1a8221e09
USER <USER@DESKTOP-R46UJVN>
2024-11-27 12:08:59 +0530

    updated the handball for the second time

--- TestingProject/HandBall.java
+++ TestingProject/HandBall.java
@@ -2,8 +2,8 @@
 public class HandBall {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		
+		System.out.println("play");
+		System.out.println("dont play");
+}
 }
